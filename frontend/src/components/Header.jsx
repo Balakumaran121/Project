033a933843602c54909892ea6c7a9c6df2a70687
vishwajith <vishwakr1998@gmail.com>
2025-03-21 18:32:35 +0530

    bg color is changed in header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ import Logout from '../assets/images/shutdown.svg'
 const Header = ({ setOpenCreatePopop }) => {
   const { logout } = useAuthStore()
   return (
-    <div className='flex justify-between items-center h-20 mx-auto text-white px-12 bg-blue-500'>
+    <div className='flex justify-between items-center h-20 mx-auto text-white px-12 bg-blue-500/60'>
       <h1 className='text-4xl font-semibold tracking-wider '>Tasks List</h1>
       <div className='flex space-x-4 items-center'>
         <span  onClick={() => logout()} className='p-1.5 w-10 h-10 border-2 border-blue-500/90 bg-white cursor-pointer rounded-full  ' title='Logout'>
